4e137fa72f58b20ab86990a158a675912f3823c9
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2023-03-07 23:07:47 -0300

    justify students in page home

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=60)
+    roll = models.IntegerField(default='')
+    city = models.CharField(max_length=100)
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Student
 
 # Create your views here.
 
 
 class Home(View):
     def get(self, request):
-        return render(request, 'core/home.html')
+        stu_data = Student.objects.all()
+        return render(request, 'core/home.html', {'studata':stu_data})
