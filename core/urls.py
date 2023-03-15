b166896a9c3010bf9fe66be7c88fab788a592509
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2023-03-14 21:37:18 -0300

    add method delete in crud project

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, Add_Student
+from .views import Home, Add_Student, Delete_Student
 
 urlpatterns = [
     path('', Home.as_view(), name= 'home'),
-    path('add-student/', Add_Student.as_view(), name='add-student')
+    path('add-student/', Add_Student.as_view(), name='add-student'),
+    path('delete-student', Delete_Student.as_view(), name='delete-student')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from .models import Student
+from .forms import AddStudentForm
 
 # Create your views here.
 
@@ -13,4 +14,24 @@ class Home(View):
 
 class Add_Student(View):
     def get(self, request):
-        return render(request, 'core/add-student.html')
+        fm = AddStudentForm()
+        return render(request, 'core/add-student.html', {'form':fm})
+    
+    def post(self, request):
+        fm = AddStudentForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            return redirect('/')
+            
+        else:
+            return render(request, 'core/add-student.html', {'form':fm})
+
+
+class Delete_Student(View):
+    def post(self, request):
+        data = request.POST
+        id = data.get('id')
+        studata = Student.objects.get(id = id)
+        studata.delete()
+        return redirect('/')
+        
